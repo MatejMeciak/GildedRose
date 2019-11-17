8c6e54b8b95f1b2aa4bed8ad4943da3360a8d8f2
Matej Meciak <matej.meciak123@gmail.com>
2019-11-17 21:44:41 +0100

    Done.

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -22,8 +22,41 @@ namespace GildedRose
         /// </summary>
         public void UpdateQuality()
         {
-            // TODO ...
-            // Hint: Iterate through this.items and check Name property to access specific item
+            foreach (var item in items)
+            {
+                var qualilityChangeVal = 1;
+                if (item.SellIn <= 0) qualilityChangeVal = 2;
+
+                if (item.Name == "+5 Dexterity Vest")
+                    item.Quality -= qualilityChangeVal;
+
+                else if (item.Name == "Aged Brie")
+                    item.Quality += qualilityChangeVal;
+
+                else if (item.Name == "Elixir of the Mongoose")
+                {
+
+                    item.Quality -= qualilityChangeVal;
+                }
+
+                else if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    continue;
+
+                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn <= 0) item.Quality = 0;
+                    else if (item.SellIn < 6) item.Quality += 3;
+                    else if (item.SellIn < 11) item.Quality += 2;
+                    else item.Quality += 1;
+                }
+
+                else if (item.Name == "Conjured Mana Cake")
+                    item.Quality -= qualilityChangeVal * 2;
+
+                if (item.Quality < 0) item.Quality = 0;
+                if (item.Quality > 50) item.Quality = 50;
+                item.SellIn--;
+            }
         }
     }
 }
